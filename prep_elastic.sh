6185cc8730ae444c54914d758699242032c1b544
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2024-04-18 15:43:23 -0500

    changes to fix ingestion

--- prep_elastic.sh
+++ prep_elastic.sh
@@ -1,6 +1,6 @@
 export PYTHONPATH="$PWD:$PYTHONPATH"
 input=${1:-configs/ibm_config.yaml}
-python3.10 elastic/setup.py --config_file_path $input -s
+python3 elastic/setup.py --config_file_path $input -s
 # Ingest documents into elasticsearch
-python3.10 elastic/ingest.py --config_file_path $input
+python3 elastic/ingest.py --config_file_path $input
 
